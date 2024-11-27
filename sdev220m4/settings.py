4e3be355da638222179aea1b958773cb274edcac
Hr0thmund <lfawkes@ivytech.edu>
2024-11-27 17:52:27 -0500

    Update settings.py
    Obfuscating secret key

--- sdev220m4/settings.py
+++ sdev220m4/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+xtm78kx1zf8ncjbg$$cw_0j^+p5f4f47qci7b7a!(s#ipc2j^'
+SECRET_KEY = [redacted]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
